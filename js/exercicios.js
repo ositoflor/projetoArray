f3d70dd605c42e5deb3c094783e2472b2b52e8b9
unknown <netojose42@gmail.com>
2022-02-15 16:19:18 -0300

    Implementação Exercicíos

---
+++ js/exercicios.js
@@ -0,0 +1,20 @@
+function resposta1(){
+    let resp = $("#resposta").val()
+    if(resp === "carros[1]"){
+        $("#btn-proximo1").removeClass("sumir-btn");
+        $("#result").html("Parabéns você acertou!! 🤩🤩");
+    } else {
+        return $("#result").html("Poxa não acertou  😔😔 tente novamente.");
+    }
+}
+
+
+function resposta2(){
+    let resp = $("#resposta").val()
+    if(resp === "frutas.pop()"){
+        $("#btn-proximo1").removeClass("sumir-btn");
+        $("#result").html("Parabéns você acertou!! 🤩🤩");
+    } else {
+        return $("#result").html("Poxa não acertou 😔😔 tente novamente.");
+    }
+}
---
+++ js/routes.js
@@ -0,0 +1,15 @@
+function array() {
+    $("#content").load("./pages/array.html");
+  }
+
+  function filter() {
+    $("#content").load("./pages/filter.html");
+  }
+
+  function exercicio() {
+    $("#content").load("./pages/exercicio.html");
+  }
+
+  function exercicio2() {
+    $("#content").load("./pages/exercicio2.html");
+  }
